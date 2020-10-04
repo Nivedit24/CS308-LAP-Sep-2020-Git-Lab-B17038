cbd97ac1e778b8aac81e9daa99038b21a9a25968
Nivedit Reddy <bniveditreddy@gmail.com>
2020-10-04 12:30:59 +0530

    Added i for Negative Square Roots

--- sqrt.c
+++ sqrt.c
@@ -15,10 +15,9 @@ int main(int argc, char* argv[]) {
 	int input = atoi(argv[1]);
 	if(input<0)
 	{
-		printf("Input must be positive. Enter a valid number\n");
-		return 1;
+		printf("Sqrt of %d is %fi\n", input, sqrt(-input));
 	}
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
+	else printf("Sqrt of %d is %f\n",input,sqrt(input));
 	printf("End of program. Exiting.\n");
 	return (0);
 
